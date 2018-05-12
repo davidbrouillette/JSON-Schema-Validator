dfa2a57480845bde5b23e2f35d0090901343535d
brouilld <brouilld@oregonstate.edu>
2018-05-12 11:13:26 -0700

    Added tests for stringtype

--- tests/string.test.js
+++ tests/string.test.js
@@ -1,6 +1,18 @@
 import StringType from "../src/types/string";
 
 describe("StringType tests", () => {
+    it("'isString' evaluates true", () => {
+        let obj = new StringType();
+        let result = obj.isString("thisIsAString");
+        expect(result).toBeTruthy();
+    });
+
+    it("'isString' evaluates false", () => {
+        let obj = new StringType();
+        let result = obj.isString(/[A-Z]/);
+        expect(result).toBeFalsy();
+    });
+
     it("'test' validates as correct", () => {
         let obj = new StringType();
         let result = obj.runValidators("test");
@@ -50,8 +62,22 @@ describe("StringType tests", () => {
 
     it("matchesPattern('sting') & value='Testing' validates as correct", () => {
         let obj = new StringType();
-        obj.matchesPattern('sting');
+        obj.matchesPattern("sting");
         let result = obj.runValidators("Testing");
         expect(result).toBeTruthy();
+    });
+
+    it("matchesPattern(/([A-Z])/) & value='testing' validates as error", () => {
+        let obj = new StringType();
+        obj.matchesPattern(/([A-Z])/);
+        let result = obj.runValidators("testing");
+        expect(result).toBeFalsy();
+    });
+
+    it("matchesPattern('sting') & value='tired' validates as error", () => {
+        let obj = new StringType();
+        obj.matchesPattern("sting");
+        let result = obj.runValidators("tired");
+        expect(result).toBeFalsy();
     });
 });
